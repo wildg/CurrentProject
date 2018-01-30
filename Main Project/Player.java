361350265586a2706aa23b121f26628d9b38baa0
brandon-a-fisher <35775975+brandon-a-fisher@users.noreply.github.com>
2018-01-30 11:05:32 -0700

    Add files via upload

---
+++ Main Project/Player.java
@@ -0,0 +1,31 @@
+public class Player
+{
+	private int playerHealth = 5;
+	private int xPos = 0;
+	private int yPos = 0;
+
+	public boolean IsAlive()
+	{
+		if(playerHealth<=0)
+		{
+			return False;
+		}
+		else
+		{
+			return True;
+		}
+	}
+
+	public void TakeDamage()
+	{
+		playerHealth -= 1;
+	}
+
+	public void Heal()
+	{
+		if(playerHealth < 5)
+		{
+			playerHealth += 1;
+		}
+	}
+}
