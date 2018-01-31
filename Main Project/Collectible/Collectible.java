5d2dcf1c998d9eff638eaf705245f18ad08add04
BluSnowY <35359631+BluSnowY@users.noreply.github.com>
2018-01-31 09:47:40 -0700

    Add files via upload

--- Main Project/Collectible/Collectible.java
+++ Main Project/Collectible/Collectible.java
@@ -7,8 +7,8 @@ public class Collectible {
     Random rand = new Random();
     private double n = rand.nextInt(maxHeight) + 1;
 
-    public boolean overlapsWith(avatar: Avatar){
-        if (Avatar.x == this.x && Avatar.y == this.y)
+    public boolean overlapsWith(Avatar avatar){
+        if (avatar.x == this.x && avatar.y == this.y)
             return true;
         else
             return false;
