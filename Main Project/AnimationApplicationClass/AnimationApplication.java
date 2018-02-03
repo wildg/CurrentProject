a87a416cc2c6eb40eeb1f473d851c92c86c35d09
Mark <35700344+markjt17@users.noreply.github.com>
2018-02-03 15:29:36 -0700

    Version25

--- Main Project/AnimationApplicationClass/AnimationApplication.java
+++ Main Project/AnimationApplicationClass/AnimationApplication.java
@@ -4,13 +4,13 @@
 	
 	// 
 	
-	/* 
-	Version 24
-	1. Updated some implementation comments.
-	2. Created MAX_OBJECTS_GAME instance variable.
-	3. Started game run algorithm in main method.
-	4. Created getPlayer(), getCollectible() and getObstacle() functions.
-	**/
+  /** 
+	*Version 25
+	* 1. Overhauled registerCollison() so that now its registerCollsionPlayer() and handles
+	*    collisions with players for demo 1.
+	* 2. Removed debug else in make methods.
+	* 3. Made properly javaDoc compatible.
+	*/
 
 public class AnimationApplication
 {
@@ -31,7 +31,9 @@ public class AnimationApplication
 	Player[] activePlayerList = new Player[MAX_ACTIVE_PLAYERS];
 	String[] activeCollectibleList= new String[MAX_ACTIVE_COLLECTIBLES]; // Will use String for Array type until Collectible class imported.
 	
-	/*Places newly instantiated obstacles in the first free index of the activeObstacleList**/
+	/**
+	  *Places newly instantiated obstacles in the first free index of the activeObstacleList.
+	  */
 	private void addToActiveObstacleList(String myObject)
 	{
 		for(int index=0; index < activeObstacleList.length; index++)
@@ -44,7 +46,9 @@ public class AnimationApplication
 		}
 	}
 	
-	/*Places newly instantiated players in the first free index of the activePlayerList**/
+   /** 
+	 * Places newly instantiated players in the first free index of the activePlayerList.
+	 */
 	private void addToActivePlayerList(Player myObject)
 	{
 		for(int index=0; index < activePlayerList.length; index++)
@@ -57,7 +61,9 @@ public class AnimationApplication
 		}
 	}
 	
-	/*Places newly instantiated collectibles in the first free index of the activeCollectibleList**/
+	/** 
+	  *Places newly instantiated collectibles in the first free index of the activeCollectibleList.
+	  */
 	private void addToActiveCollectibleList(String myObject)
 	{
 		for(int index=0; index < activeCollectibleList.length; index++)
@@ -70,10 +76,11 @@ public class AnimationApplication
 		}
 	}
 
-	/*Prints out contents of one of the Active Object Lists corresponding 
-	  to the String that the method takes as an argument. Valid String
-      arguments are as follows: "Obstacle","Collectible", and "Player".
-	  Case matters.**/
+	/** Prints out contents of one of the Active Object Lists corresponding 
+	  * to the String that the method takes as an argument. Valid String
+      * arguments are as follows: "Obstacle","Collectible", and "Player".
+	  * Case matters.
+	  */
 	public void printActiveObjectList(String whichList)
 	{
 		boolean isObstacle = whichList.equals("Obstacle");
@@ -145,9 +152,10 @@ public class AnimationApplication
 		System.out.println("________________________________________________________");
 	}
 	
-	/*Instantiates an Obstacle at the specified x and y positions on the map. Takes
-	  two integers representing the x and y positions as arguments.**/
-	private void makeObstacle(int xpos, int ypos)
+	/** Instantiates an Obstacle at the specified x and y positions on the map. Takes
+	  * two integers representing the x and y positions as arguments.
+	  */
+	public void makeObstacle(int xpos, int ypos)
 	{
 		int maximumActive = MAX_ACTIVE_OBSTACLES;
 		int currentActive = numActiveObstacles;
@@ -166,19 +174,13 @@ public class AnimationApplication
 			// reallocate the memory for the unreferenced object, preventing
 			// garbage data building up. Same goes for other make methods.
 		}
-		
-		// else // for debugging
-		// {
-			 // System.out.println("");
-			 // System.out.println("Can't instantiate anymore obstacles, max exceeded"); 
-			 // System.out.println("Current instantiated obstacles " + "= " + numActiveObstacles);
-			 // System.out.println("");
-		// }
 	}
 	
-	/*Instantiates a Player at the specified x and y positions on the map. Takes
-	  two integers representing the x and y positions as arguments.**/
-	private void makePlayer(int xpos, int ypos) 
+	/** 
+	  * Instantiates a Player at the specified x and y positions on the map. Takes
+	  * two integers representing the x and y positions as arguments.
+	  */
+	public void makePlayer(int xpos, int ypos) 
 	{
 		int maximumActive = MAX_ACTIVE_PLAYERS;
 		int currentActive = numActivePlayers;
@@ -190,19 +192,12 @@ public class AnimationApplication
 			numActivePlayers++;
 			addToActivePlayerList(playerObject);
 		}
-		
-		// else // for debugging
-		// {
-			 // System.out.println("");
-			 // System.out.println("Can't instantiate anymore players, max exceeded"); 
-			 // System.out.println("Current instantiated players " + "= " + numActivePlayers);
-			 // System.out.println("");
-		// }
 	}
 	
-	/*Instantiates a Collectible at the specified x and y positions on the map. Takes
-	  two integers representing the x and y positions as arguments.**/
-	private void makeCollectible(int xpos, int ypos) 
+   /** Instantiates a Collectible at the specified x and y positions on the map. Takes
+	 * two integers representing the x and y positions as arguments.
+	 */
+	public void makeCollectible(int xpos, int ypos) 
 	{
 		int maximumActive = MAX_ACTIVE_COLLECTIBLES;
 		int currentActive = numActiveCollectibles;
@@ -214,18 +209,12 @@ public class AnimationApplication
 			numActiveCollectibles++;
 			addToActiveCollectibleList(collectibleObject);
 		}
-		
-		// else // for debugging
-		// {
-			 // System.out.println("");
-			 // System.out.println("Can't instantiate anymore collectibles, max exceeded"); 
-			 // System.out.println("Current instantiated collectibles " + "= " + numActiveCollectibles);
-			 // System.out.println("");
-		// }
 	}
 	
-	/* Removes obstacle at specified index from activeObstacleList by putting a null value in its place. **/
-	private void deleteObstacle(int index) 
+	/**  
+	  * Removes obstacle at specified index from activeObstacleList by putting a null value in its place. 
+	  */
+	public void deleteObstacle(int index) 
 	{
 		if (index < activeObstacleList.length && index >= 0)
 		{
@@ -234,8 +223,10 @@ public class AnimationApplication
 		}	
 	}
 	
-	/* Removes player at specified index from activePlayerList by putting a null value in its place. **/
-	private void deletePlayer(int index) 
+   /** 
+	 * Removes player at specified index from activePlayerList by putting a null value in its place. 
+	 */
+	public void deletePlayer(int index) 
 	{
 		if (index < activePlayerList.length && index >= 0)
 		{
@@ -244,8 +235,10 @@ public class AnimationApplication
 		}	
 	}
 	
-	/* Removes obstacle at specified index from activeObstacleList by putting a null value in its place. **/
-	private void deleteCollectible(int index) 
+	/**  
+	  * Removes obstacle at specified index from activeObstacleList by putting a null value in its place. 
+	  */
+	public void deleteCollectible(int index) 
 	{
 		if (index < activeCollectibleList.length && index >= 0)
 		{
@@ -254,95 +247,74 @@ public class AnimationApplication
 		}	
 	}
 	
-	/*Checks all active objects of a certain type, as specified by a String argument, for
-	  overlap with an active object it can collide with, and takes necessary action if 
-	  this is true. Valid String arguments include: "Obstacle", "Player" and "Collectible".
-	  Case matters. As of the current all active objects of a chosen type are deleted by
-	  the algorithm, but this will be changed to the former definition in a later 
-	  implementation.**/
-	private void registerCollision(String whichType)
-	{
-		boolean isObstacle = whichType.equals("Obstacle");
-		boolean isPlayer = whichType.equals("Player");
-		boolean isCollectible = whichType.equals("Collectible");
-		
-		boolean collisionHappened = false;
-		String collisionWithWhat = "None";
-		
-		Object[]listToCheck; 
-		listToCheck = new Object[1]; // Arbitrary Array size. Just need to initialize
-		
-		int numActive = 0;
-		
-		if(isObstacle)
+	/** 
+	  * Checks all active player objects of a certain type for collisions with valid active objects, as specified
+	  * by a String argument, and takes necessary action if this is true. As of the current this method is not fully 
+	  * implemented. Valid arguments for String include "Obstacle", and "Collectible". Case matters.
+	  */
+	private void registerCollisionPlayer(String activeObjectType)
+	{	
+		if(activeObjectType == "Obstacle")
 		{
-			listToCheck = new String[1];
-			listToCheck = activeObstacleList;
-			numActive = numActiveObstacles;
+			if (numActivePlayers > 0 & numActiveObstacles > 0)
+			{
+				for(int playerIndex = 0; playerIndex < activePlayerList.length; playerIndex++) // Run through each index of activePlayerList
+				{
+					//getPlayer(playerIndex).hasCollisionObstacle <Obstacle[]> <gameEngine>
+					
+																							 // Method should check for collisions with objects
+																						     // in Obstacle[] given as argument, and take 
+																							 // necessary action using AnimationApplication
+																							 // argument; ie: gameEngine.deleteObstacle(index).
+				}
+			}
 		}
 		
-		else if(isPlayer)
+		else if(activeObjectType == "Collectible")
 		{
-			listToCheck = new Player[1];
-			listToCheck = activePlayerList;
-			numActive = numActivePlayers;
-		}
-		
-		else if(isCollectible)
-		{
-			listToCheck = new String[1];
-			listToCheck = activeCollectibleList;
-			numActive = numActiveCollectibles;
+			if (numActivePlayers > 0 & numActiveCollectibles > 0)
+			{
+				for(int playerIndex = 0; playerIndex < activePlayerList.length; playerIndex++) // Run through each index of activePlayerList
+				{
+					//getPlayer(playerIndex).hasCollisionCollectible <Collectible[]> <gameEngine> 
+					
+																								   // Method should check for collisions with objects
+																								   // in Collectible[] given as argument, and take 
+																								   // necessary action using AnimationApplication
+																								   // argument; ie: gameEngine.deleteObstacle(index).
+				}
+			}
 		}
 		
 		else
 		{
 			System.out.println("ERROR: Please enter a valid String argument for");
-			System.out.println("the registerCollision method: " + "'" + whichType + "'");
+			System.out.println("the registerPlayerCollision method: " + "'" + activeObjectType + "'");
 			System.out.println("is not a valid argument.");
-			System.exit(0);
-		}
-		
-		if (numActive > 0)
-		{
-			for(int index = 0; index < listToCheck.length;index++) 
-			{
-				if(listToCheck[index] != null)
-					System.out.print("");
-					// collisionHappened = listToCheck[index].didOverlapOccur()(note this overlapMethod should return true or false)
-				else
-					continue;
-				
-				if (collisionHappened)
-				{
-					if(isObstacle)
-						deleteObstacle(index);
-					
-					else if(isCollectible)
-						deleteCollectible(index);
-					
-					else if(isPlayer)
-						// collisionWithWhat = listToCheck.overlapWithType()(note this method should return a String)
-						getPlayer(index).TakeDamage();
-				}
-			}
+			System.exit(0);	
 		}
 	}
 	
-	/*Returns Player object at specified index. **/
-	private Player getPlayer(int index)
+	/** 
+	  * Returns Player object at specified index.
+	  **/
+	public Player getPlayer(int index)
 	{
 		return activePlayerList[index];
 	}
 	
-	/*Returns Obstacle object at specified index. **/
-	private String getObstacle(int index)
+	/** 
+	  *Returns Obstacle object at specified index.
+	  */
+	public String getObstacle(int index)
 	{
 		return activeObstacleList[index];
 	}
 	
-	/*Returns Collectible object at specified index. **/
-	private String getCollectible(int index)
+	/** 
+	  * Returns Collectible object at specified index. 
+	  */
+	public String getCollectible(int index)
 	{
 		return activeCollectibleList[index];
 	}
@@ -356,18 +328,15 @@ public class AnimationApplication
 		gameEngine.makeObstacle(30,10); // So we have at least one obstacle when game starts.
 		
 		boolean gameOver = (gameEngine.numActivePlayers < 0);
-		
-		// gameEngine.printActiveObjectList("Player");
-		// gameEngine.printActiveObjectList("Obstacle");
-		
+	
 		while(!(gameOver))
-		{
-			gameEngine.registerCollision("Player");
-			gameEngine.registerCollision("Obstacle");
-			gameEngine.registerCollision("Collectible");
+		{	
+			System.out.println("");
+			System.out.println("IS PLAYER ONE ALIVE: " + playerOne.IsAlive() );
 			
-			// gameEngine.printActiveObjectList("Player");
-			// gameEngine.printActiveObjectList("Obstacle");
+			gameEngine.printActiveObjectList("Player");
+			gameEngine.printActiveObjectList("Obstacle");
+			gameEngine.printActiveObjectList("Collectible");
 			
 			gameOver = true;
 		}
