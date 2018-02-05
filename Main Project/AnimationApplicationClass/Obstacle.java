2809348eeb031e19c6f4b2044e761a0cb99c83b6
wildg <35607715+wildg@users.noreply.github.com>
2018-02-04 21:04:37 -0700

    Update Obstacle.java

--- Main Project/AnimationApplicationClass/Obstacle.java
+++ Main Project/AnimationApplicationClass/Obstacle.java
@@ -1,4 +1,29 @@
-public class Obstacle
-{
+public class Obstacle {
+	private static int length = 1;
+			
+	private static int yValue = 1;	//Here, the values of length and height of the
+	private static int xValue = 10;
 	
+	private void moveObstaclePosition()
+	{
+		xValue -= 1;
+	}
+	
+	private int getObstaclePosition()
+	{
+		return xValue;
+	}
+	
+	private boolean hitObstacle( int xPos, int yPos )
+	{
+		if ( (xValue == xPos) && (yValue == yPos ) )
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
 }
